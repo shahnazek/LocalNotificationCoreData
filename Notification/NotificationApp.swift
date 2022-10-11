d5b1415b5ae3dbebdd5709f73af811a77e9bfa3b
Shahnaz <ekshahnaz@gmail.com>
2022-10-11 15:06:26 +0530

    Initial Commit

---
+++ Notification/NotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationApp.swift
+//  Notification
+//
+//  Created by Shahnaz EK on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct NotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
